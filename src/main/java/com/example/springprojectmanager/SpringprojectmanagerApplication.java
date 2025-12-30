dc0d379448120a2a4c69ab4801c8d4d75b366e85
Pedro Carvalho <pedro8.carvalho@gmail.com>
2025-12-30 16:26:13 -0300

    criacao da estrutura basica do projeto

---
+++ src/main/java/com/example/springprojectmanager/SpringprojectmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springprojectmanager/SpringprojectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
